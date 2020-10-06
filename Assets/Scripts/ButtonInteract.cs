351ae7f9444495d2b1bb22a222d05fb0e74eaae1
DonovanMontoya <donovan.montoya1@gmail.com>
2020-10-06 00:22:24 -0600

    Overhaul One
    - Added Snap Sockets
    - Updated Workspace
    - Added Buttons

---
+++ Assets/Scripts/ButtonInteract.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.Events;
+
+public class ButtonInteract : XRBaseInteractable
+{
+    public UnityEvent OnPress = null;
+
+
+    private XRBaseInteractor hoverInteractor;
+    private float previousHandHight = 0.0f;
+    private float yMin = 0.0f;
+    private float yMax = 0.0f;
+    private bool previousPress = false;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        onHoverEnter.AddListener(StartPress);
+        onHoverExit.AddListener(EndPress);
+    }
+
+    private void OnDestroy()
+    {
+        onHoverEnter.RemoveListener(StartPress);
+        onHoverExit.RemoveListener(EndPress);
+    }
+
+    private void StartPress(XRBaseInteractor interactor)
+    {
+        hoverInteractor = interactor;
+        previousHandHight = GetLocalYPosition(hoverInteractor.transform.position);
+    }
+
+    private void EndPress(XRBaseInteractor interactor)
+    {
+        hoverInteractor = null;
+        previousHandHight = 0.0f;
+        previousPress = false;
+        SetYPosition(yMax);
+    }
+
+    private void Start()
+    {
+        SetMinMax();
+    }
+
+    private void SetMinMax()
+    {
+        Collider collider = GetComponent<Collider>();
+        yMin = transform.localPosition.y - (collider.bounds.size.y * 0.5f);
+        yMax = transform.localPosition.y;
+    }
+
+    public override void ProcessInteractable(XRInteractionUpdateOrder.UpdatePhase updatePhase)
+    {
+        if (hoverInteractor)
+        {
+            float newHandHight = GetLocalYPosition(hoverInteractor.transform.position);
+            float handDifference = previousHandHight - newHandHight;
+            previousHandHight = newHandHight;
+
+            float newPosition = transform.localPosition.y - handDifference;
+            SetYPosition(newPosition);
+
+            CheckPress();
+        }
+    }
+
+    private float GetLocalYPosition(Vector3 position)
+    {
+        Vector3 localPosition = transform.root.InverseTransformPoint(position);
+        return localPosition.y;
+    }
+
+    private void SetYPosition(float position)
+    {
+        Vector3 newPosition = transform.localPosition;
+        newPosition.y = Mathf.Clamp(position, yMin, yMax);
+        transform.localPosition = newPosition;
+    }
+
+    private void CheckPress()
+    {
+        bool inPosition = InPosition();
+
+        if (inPosition && inPosition != previousPress)
+        {
+            OnPress.Invoke();
+
+            previousPress = inPosition;
+        }
+    }
+
+    private bool InPosition()
+    {
+        float inRange = Mathf.Clamp(transform.localPosition.y, yMin, yMin + 0.01f);
+        return transform.localPosition.y == inRange;
+    }
+}
