ce65bbcf5a322c18bd69c198e07f25a4cb30a2bc
Donovan Montoya <DONOVANMONTOYA@boisestate.edu>
2021-04-30 00:23:07 -0600

    Further Functional Decoration
    Added Background Robots
    Added Custom Sounds
    Added Visualizer Jukebox

---
+++ Assets/Visualizer/Scripts/VisualScaler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+
+public class VisualScaler : MonoBehaviour
+{
+    public float scale;
+    public int bin;
+
+    void Update()
+    {
+        //original code
+        /*
+        var val = VisualizeSoundManager.Instance.bins[bin] * scale;
+        transform.localScale = new Vector3(1,val, 1);
+        */
+
+        GetComponent<Renderer>().material.color = Color.white;
+
+        Color val = new Color
+            (VisualizeSoundManager.Instance.bins[bin] * Random.value * scale, 
+            VisualizeSoundManager.Instance.bins[bin] * Random.value * scale, 
+            VisualizeSoundManager.Instance.bins[bin] * Random.value * scale);
+        GetComponent<Renderer>().material.color = val;
+        
+    }
+}
+
