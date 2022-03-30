20019cdd43098b06389971266574b7573d4b40bc
Donovan Montoya <donovan.montoya1@gmail.com>
2022-03-29 21:11:13 -0600

    Organization and Texture Updates
    - Organized Texture and Material Files
    - Replaced All Non-Original textures

---
+++ Assets/Scripts/Game Management/CameraHeightScaler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraHeightScaler : MonoBehaviour
+{
+    [SerializeField]
+    private float defaultHeight = 1.8f;
+    [SerializeField]
+    private Camera camera;
+    
+    private void Resize()
+    {
+        float headHeight = camera.transform.position.y;
+        float scale = defaultHeight / headHeight;
+        transform.localScale = Vector3.one * scale;
+    }
+
+    void OnEnable()
+    {
+        Resize();
+    }
+}
